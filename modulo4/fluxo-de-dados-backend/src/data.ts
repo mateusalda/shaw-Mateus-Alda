ad5846a9c6065acd31bfff6190407745681a26c9
Mateus <mateus.a.alda@gmail.com>
2022-06-01 18:09:15 -0300

    atividade fluxo de dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        id: '0p',
+        name: 'palha',
+        price: 5
+    },
+    {
+        id: '1t',
+        name: 'tudo',
+        price: 7
+    },
+    {
+        id: '2c',
+        name: 'coisa',
+        price: 2
+    }
+]
